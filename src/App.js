20262125a840455d3727d88666b0b2a79403b027
Fernando Stadler <fernandostadler2@gmail.com>
2020-06-17 23:24:41 -0300

    Criação do projeto JsHunt da rocketseat

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Routes from './routes'
+
+
+import './style.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Main from './pages/main/'
+import { Route } from "react-router-dom";
+
+
+const App = () => (
+
+  <div className="App">
+     <Header />
+     <Routes />
+     <Footer/>
+      </div>
+
+
+);
+
+
+export default App;
---
+++ src/Components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import './style.css'
+const Footer = () => <footer id="main-footer">Fernando Stadler 2020 | 
+<a href="https://github.com/fernandoStadler" target="_blank"> Github | </a> 
+<a href="http://api.whatsapp.com/send?1=pt_BR&phone=5521993608648." target="_blank"> WhatsApp | </a> 
+<a href="https://www.linkedin.com/in/fernando-stadler-9a6727131/" target="_blank">Linkedin</a>
+</footer>;
+
+export default Footer;
